dafde3ff1fa5e4efb4babecc73a93c4e9b647bf6
Conrad Lukawski <conrad@backlight.ca>
2020-12-21 10:46:55 -0500

    Handle CIDR ingestion and create IpList struct/impl for allocation

---
+++ src/device/ip_list.rs
@@ -0,0 +1,60 @@
+extern crate cidr;
+
+use cidr::{Cidr, Ipv4Cidr};
+use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+use std::vec::Vec;
+
+struct IpList {
+    pub list: RefCell<Vec<[u8; 5]>>,
+    pub allocated: Cell<HashMap<[u8; 5], bool>>,
+    index: Cell<usize>,
+}
+
+impl IpList {
+    pub fn new(cidr: [u8; 5]) -> Result<IpList, Error> {
+        let subnet: u8 = cidr[4];
+        // It can also be created from string representations.
+        let net = Ipv4Cidr::new(Ipv4Addr::new(cidr[0], cidr[1], cidr[2], cidr[3]), subnet)
+            .expect("Expected valid ip range");
+
+        let mut ip_list: Vec<[u8; 5]> = Vec::new();
+        for ip in net.iter() {
+            let octets = ip.octets();
+            if octets[3] == 255 || octets[3] == 0 {
+                continue;
+            }
+            let full_ip: [u8; 5] = [octets[0], octets[1], octets[2], octets[3], subnet];
+            ip_list.push(full_ip);
+        }
+    }
+
+    pub fn get_ip(&mut self) -> Option<[u8; 5]> {
+        let mut current = self.index.get();
+        let list = self.list.get_mut();
+        let mut ip_avail = false;
+        let allocated = self.allocated.get_mut();
+        let mut counter: usize = 0;
+
+        while !ip_avail {
+            current = self.index.get();
+            if counter == list.len() {
+                return None;
+            }
+
+            self.index.set(current + 1);
+            let ip = list[self.index.get()];
+            ip_avail = !allocated.contains_key(&ip);
+            counter += 1;
+        }
+
+        return Some(list[self.index.get()]);
+    }
+
+    pub fn allocate_ip(&mut self, ip: [u8; 5]) {
+        let allocated = self.allocated.get_mut();
+
+        allocated.insert(ip, true);
+    }
+}
